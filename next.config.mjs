02bfe69dccc7a7f0efd19444649cdb1e12e41e6d
Angelo Jose <angelo-mmacedo@hotmail.com>
2026-05-07 20:22:17 +0100

    Fix next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
-/** @type {import('next').NextConfig} */
+
+
 const nextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/NOME_DO_REPOSITORIO',
+  assetPrefix: '/NOME_DO_REPOSITORIO/',
 };
 
-export default nextConfig;
+module.exports = nextConfig;
